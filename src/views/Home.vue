198c0bf0a4131d4f18496c3d9056ebf761f9d14e
hijrahassalam <hijrahassalam@student.uns.ac.id>
2022-02-08 15:04:08 +0700

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-shayna />
+    <carousel-shayna />
+    <item-shayna />
+    <insta-shayna />
+    <partner-shayna />
+    <footer-shayna />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue";
+//import carousel from "vue-owl-carousel";
+import CarouselShayna from "../components/CarouselShayna.vue";
+import ItemShayna from "../components/ItemShayna.vue";
+import InstaShayna from "../components/InstaShayna.vue";
+import PartnerShayna from "../components/PartnerShayna.vue";
+import FooterShayna from "../components/FooterShayna.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HeaderShayna,
+    //carousel,
+    CarouselShayna,
+    ItemShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  },
+};
 </script>
